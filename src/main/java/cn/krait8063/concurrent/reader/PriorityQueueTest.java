b83d193b0fc9650fb43bd157c0005efa7744549c
zhangshiqi03 <zhangshiqi03@meituan.com>
2019-12-17 08:49:45 +0800

    lrucache

---
+++ src/main/java/cn/krait8063/concurrent/reader/PriorityQueueTest.java
@@ -0,0 +1,21 @@
+package cn.krait8063.concurrent.reader;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+/**
+ * created by krait8064 at 2019/12/16
+ */
+public class PriorityQueueTest {
+    @Test
+    public void test() {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(2, Comparator.comparingInt(Integer::intValue));
+        queue.add(3);
+        queue.add(2);
+        queue.add(1);
+        Integer poll = queue.poll();
+    }
+}
