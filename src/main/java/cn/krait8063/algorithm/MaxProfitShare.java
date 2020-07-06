ecf8aff2f1740ebc96a99c68cc6b1606db49020b
krait8064 <krait8063@163.com>
2020-07-06 22:54:08 +0800

    max share2

--- src/main/java/cn/krait8063/algorithm/MaxProfitShare.java
+++ src/main/java/cn/krait8063/algorithm/MaxProfitShare.java
@@ -1,21 +1,43 @@
 package cn.krait8063.algorithm;
 
+import org.junit.Test;
+
 /**
  * created by krait8064 at 2020/07/06
  */
 public class MaxProfitShare {
-    public class Solution {
-        public int maxProfit(int prices[]) {
-            int minprice = Integer.MAX_VALUE;
-            int maxprofit = 0;
-            for (int i = 0; i < prices.length; i++) {
-                if (prices[i] < minprice)
-                    minprice = prices[i];
-                else if (prices[i] - minprice > maxprofit)
-                    maxprofit = prices[i] - minprice;
-            }
-            return maxprofit;
+
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
         }
+        return maxprofit;
+    }
+
+    public int maxProfit2(int prices[]) {
+        int result = 0;
+        if (prices != null && prices.length > 1) {
+            int end = 1;
+            while (end < prices.length) {
+               if (prices[end] > prices[end-1]) {
+                    result += prices[end ] - prices[end-1];
+                }
+                end++;
+            }
+        }
+        System.out.println("rsult:"+result);
+        return result;
+    }
+
+    @Test
+    public void test() {
+        int[] arr = {1, 2, 3};
+        int res = maxProfit2(arr);
     }
 
 
