b8da0b9e3424ddcbb54e11be831f79446e63ba1a
zhangshiqi03 <zhangshiqi03@meituan.com>
2020-07-06 09:20:36 +0800

    add String

---
+++ src/main/java/cn/krait8063/algorithm/StringAdd.java
@@ -0,0 +1,36 @@
+package cn.krait8063.algorithm;
+
+import org.junit.Test;
+
+/**
+ * created by krait8064 at 2020/07/06
+ */
+public class StringAdd {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        int[] res = new int[num1.length() + num2.length()];
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            int n1 = num1.charAt(i) - '0';
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                int n2 = num2.charAt(j) - '0';
+                int sum = (res[i + j + 1] + n1 * n2);
+                res[i + j + 1] = sum % 10;
+                res[i + j] += sum / 10;
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < res.length; i++) {
+            if (i == 0 && res[i] == 0) continue;
+            result.append(res[i]);
+        }
+        return result.toString();
+    }
+
+    @Test
+    public void test() {
+        String multiply = multiply("123", "45");
+    }
+}
