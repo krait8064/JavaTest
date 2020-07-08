bbb88fd92e5da6f3b0e156db00ad7c1d75ca51e1
zhangshiqi03 <zhangshiqi03@meituan.com>
2020-07-08 11:23:21 +0800

    init

--- src/main/java/cn/krait8063/algorithm/ArrayKMax.java
+++ src/main/java/cn/krait8063/algorithm/ArrayKMax.java
@@ -1,54 +1,72 @@
 package cn.krait8063.algorithm;
 
+import org.junit.Test;
+
 import java.util.Random;
 
 public class ArrayKMax {
 
 
-        private static Random random = new Random(System.currentTimeMillis());
+    private static Random random = new Random(System.currentTimeMillis());
 
-        public int findKthLargest(int[] nums, int k) {
-            int len = nums.length;
-            int target = len - k;
-            int left = 0;
-            int right = len - 1;
-            while (true) {
-                int index = partition(nums, left, right);
-                if (index < target) {
-                    left = index + 1;
-                } else if (index > target) {
-                    right = index - 1;
-                } else {
-                    return nums[index];
-                }
+    public int findKthLargest(int[] nums, int k) {
+        int len = nums.length;
+        int target = len - k;
+        int left = 0;
+        int right = len - 1;
+        while (true) {
+            int index = partition(nums, left, right);
+            if (index < target) {
+                left = index + 1;
+            } else if (index > target) {
+                right = index - 1;
+            } else {
+                return nums[index];
             }
         }
+    }
 
-        // 在区间 [left, right] 这个区间执行 partition 操作
+    // 在区间 [left, right] 这个区间执行 partition 操作
 
-        private int partition(int[] nums, int left, int right) {
-            // 在区间随机选择一个元素作为标定点
-            if (right > left) {
-                int randomIndex = left + 1 + random.nextInt(right - left);
-                swap(nums, left, randomIndex);
+    private int partition(int[] nums, int left, int right) {
+
+        int pivot = nums[left];
+        int j = left;
+        for (int i = left + 1; i <= right; i++) {
+            if (nums[i] < pivot) {
+                j++;
+                swap(nums, j, i);
             }
+        }
+        swap(nums, left, j);
+        return j;
+    }
 
-            int pivot = nums[left];
-            int j = left;
-            for (int i = left + 1; i <= right; i++) {
-                if (nums[i] < pivot) {
-                    j++;
-                    swap(nums, j, i);
-                }
+    private void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+
+    @Test
+    public void test() {
+        int[] arr = {0, 5, 2, 7, 1, 3};
+        int kthLargest = findKthLargest(arr, 2);
+        System.currentTimeMillis();
+
+    }
+
+    public int testPartition(int[] arr, int left, int right) {
+        int pri = arr[left];
+        int j = left;
+        for (int i = j + 1; i <= right; i++) {
+            if (arr[i] < pri) {
+                j++;
+                swap(arr, i, j);
             }
-            swap(nums, left, j);
-            return j;
         }
-
-        private void swap(int[] nums, int index1, int index2) {
-            int temp = nums[index1];
-            nums[index1] = nums[index2];
-            nums[index2] = temp;
-        }
+        swap(arr, j, left);
+        return j;
+    }
 
 }
