9374bce7a54781b14b0d7354972488b6db0b56a4
krait8064 <krait8063@163.com>
2022-03-22 12:19:23 +0800

    Update QuickSort.java
    init

--- src/main/java/cn/krait8063/sor/QuickSort.java
+++ src/main/java/cn/krait8063/sor/QuickSort.java
@@ -3,36 +3,33 @@ package cn.krait8063.sor;
 import org.junit.Test;
 
 public class QuickSort {
-    private void sort(int[] arr, int left, int right) {
-        if (right <= left) return;
-        int index = partition(arr, 0, arr.length - 1);
-        sort(arr, 0, index - 1);
-        sort(arr, index + 1, right);
-    }
+    void quick_sort(int s[], int l, int r)
+    {
+        if (l < r)
+        {
+            //Swap(s[l], s[(l + r) / 2]); //将中间的这个数和第一个数交换 参见注1
+            int i = l, j = r, x = s[l];
+            while (i < j)
+            {
+                while(i < j && s[j] >= x) // 从右向左找第一个小于x的数
+                    j--;
+                if(i < j)
+                    s[i++] = s[j];
 
-    @Test
-    public void test() {
-        int[] ar = {1, 4, 2, 7, 9, 3, -1};
-        sort(ar, 0, ar.length - 1);
-    }
-
-    private int partition(int[] array, int left, int right) {
-        int prio = array[left];
-        int i = left + 1;
-        int j = right;
-        while (i <= j) {
-            while (i < j && array[i] < prio) i++;
-            while (i < j && array[j] > prio) j--;
-            if (i <= j) {
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-                i++;
-                j--;
+                while(i < j && s[i] < x) // 从左向右找第一个大于等于x的数
+                    i++;
+                if(i < j)
+                    s[j--] = s[i];
             }
+            s[i] = x;
+            quick_sort(s, l, i - 1); // 递归调用
+            quick_sort(s, i + 1, r);
         }
-        array[left] = array[j];
-        array[j] = prio;
-        return j;
+    }
+
+    public static void main(String[] args) {
+        int[] ar = {1, 4, 2, 7, 9, 3, -1};
+        new QuickSort().quick_sort(ar,0,ar.length-1);
+        System.out.println(ar.toString());
     }
 }
